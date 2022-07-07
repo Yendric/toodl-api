2f429fe5e0167f9c0a515c90e4e8a37aad0437f1
Yendric <yendricvanroey@gmail.com>
2022-07-07 22:52:51 +0200

    File based session store

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import session from "express-session";
 import cors from "cors";
 import auth from "./routes/auth";
 import { config } from "dotenv";
+import SessionFileStore from "session-file-store";
 
 config();
 const API_VERSION = "v1";
 
 const app = express();
 
+const FileStore = SessionFileStore(session);
 const sessionMiddleware = session({
+  store: new FileStore(),
   secret: process.env.SECRET ?? "CHANGE-ME-IN-DOTENV-Q#%$GR$A&EHL*H@UA#RXQPSWHCDUN",
   saveUninitialized: false,
   resave: false,
