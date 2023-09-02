4ba0559b8b200bc9b3e3a07c0ad18bc04ea13943
Yendric <yendricvanroey@gmail.com>
2023-09-02 12:14:33 +0200

    Eslint cleanup

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,8 +1,8 @@
 import { ToodlError } from "@/errors/ToodlError";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
 
-function handleError(err: Error, req: Request, res: Response) {
+function handleError(err: Error, req: Request, res: Response, _next: NextFunction) {
   if (err instanceof ZodError) {
     return res.status(422).json({ message: "Validation error", errors: err.errors });
   } else if (err instanceof ToodlError) {
