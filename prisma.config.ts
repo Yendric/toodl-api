7b87e88bc1efc807741f0a1d00e05b9841502146
Yendric <yendricvanroey@gmail.com>
2026-04-14 14:29:40 +0200

    fix: ci

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,13 @@
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+expand(config());
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DB_URL"),
+    url: process.env.DB_URL ?? "",
   },
 });
