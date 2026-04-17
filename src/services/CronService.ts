7dcf18a962810c8a5375b426825e0e98ca5ccce2
Yendric <yendricvanroey@gmail.com>
2026-04-17 23:20:56 +0200

    refactor(cron): extract logic to service and optimize queries

---
+++ src/services/CronService.ts
@@ -0,0 +1,135 @@
+import { inject, injectable } from "inversify";
+import prisma from "#/prisma.js";
+import { LoggingService } from "./LoggingService.js";
+import { NotificationService } from "./NotificationService.js";
+import dayjs from "dayjs";
+
+@injectable()
+export class CronService {
+  constructor(
+    @inject(NotificationService) private notificationService: NotificationService,
+    @inject(LoggingService) private loggingService: LoggingService,
+  ) {}
+
+  public async processDailyNotifications(): Promise<void> {
+    try {
+      const tomorrowStart = dayjs().add(1, "days").startOf("day").toDate();
+      const tomorrowEnd = dayjs().add(1, "days").endOf("day").toDate();
+
+      const users = await prisma.user.findMany({
+        where: {
+          todos: {
+            some: {
+              done: false,
+              enableDeadline: true,
+              startTime: {
+                gte: tomorrowStart,
+                lte: tomorrowEnd,
+              },
+            },
+          },
+        },
+        include: {
+          todos: {
+            where: {
+              done: false,
+              enableDeadline: true,
+              startTime: {
+                gte: tomorrowStart,
+                lte: tomorrowEnd,
+              },
+            },
+          },
+          pushSubscriptions: true,
+        },
+      });
+
+      const promises = users.map((user) => {
+        if (!user.todos.length) return Promise.resolve();
+        return this.notificationService.dispatchDaily(user, user.todos);
+      });
+
+      await Promise.allSettled(promises);
+    } catch (err) {
+      this.loggingService.error("Error in daily notification cronjob: " + String(err));
+    }
+  }
+
+  public async processMinuteNotifications(): Promise<void> {
+    try {
+      const nowStart = dayjs().startOf("minute").toDate();
+      const nowEnd = dayjs().endOf("minute").toDate();
+
+      const fifteenStart = dayjs().add(15, "minute").startOf("minute").toDate();
+      const fifteenEnd = dayjs().add(15, "minute").endOf("minute").toDate();
+
+      const usersNow = await prisma.user.findMany({
+        where: {
+          todos: {
+            some: {
+              done: false,
+              enableDeadline: true,
+              startTime: {
+                gte: nowStart,
+                lte: nowEnd,
+              },
+            },
+          },
+        },
+        include: {
+          todos: {
+            where: {
+              done: false,
+              enableDeadline: true,
+              startTime: {
+                gte: nowStart,
+                lte: nowEnd,
+              },
+            },
+          },
+          pushSubscriptions: true,
+        },
+      });
+
+      const promisesNow = usersNow.flatMap((user) =>
+        user.todos.map((todo) => this.notificationService.dispatchNow(user, todo))
+      );
+
+      const usersReminder = await prisma.user.findMany({
+        where: {
+          todos: {
+            some: {
+              done: false,
+              enableDeadline: true,
+              startTime: {
+                gte: fifteenStart,
+                lte: fifteenEnd,
+              },
+            },
+          },
+        },
+        include: {
+          todos: {
+            where: {
+              done: false,
+              enableDeadline: true,
+              startTime: {
+                gte: fifteenStart,
+                lte: fifteenEnd,
+              },
+            },
+          },
+          pushSubscriptions: true,
+        },
+      });
+
+      const promisesReminder = usersReminder.flatMap((user) =>
+        user.todos.map((todo) => this.notificationService.dispatchReminder(user, todo))
+      );
+
+      await Promise.allSettled([...promisesNow, ...promisesReminder]);
+    } catch (err) {
+      this.loggingService.error("Error in minute check cronjob: " + String(err));
+    }
+  }
+}
